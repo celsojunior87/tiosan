3d0fb690637f356a2cc36fcbb21f271eb8e5cfb7
celsojunior87 <juniorcoutodf@gmail.com>
2019-09-28 11:31:30 -0300

    idjdi

--- app/Models/Servico.php
+++ app/Models/Servico.php
@@ -8,24 +8,24 @@ class Servico extends Model
 {
     protected $table = 'servicos';
 
-    protected $fillable = ['preco','adicionais','preco_adc','data_servico','hora_saida',
-        'obs_adicionais','obs_avarias','hora_entrada',
-        'categoria','funcionario',
-        'tipo_pagamento','id_cliente','id_tipo_servico','id_veiculo'];
+    protected $fillable = ['preco', 'adicionais', 'preco_adc', 'data_servico', 'hora_saida',
+        'obs_adicionais', 'obs_avarias', 'hora_entrada',
+        'categoria', 'funcionario',
+        'tipo_pagamento', 'id_cliente', 'id_tipo_servico', 'id_veiculo'];
 
     public function veiculo()
     {
-        return $this->hasOne(Veiculo::class,'id','id_veiculo');
+        return $this->hasOne(Veiculo::class, 'id', 'id_veiculo');
     }
 
     public function cliente()
     {
-        return $this->hasOne(Cliente::class,'id','id_cliente');
+        return $this->hasOne(Cliente::class, 'id', 'id_cliente');
     }
 
     public function tipoServico()
     {
-        return $this->hasOne(TipoServico::class,'id','id_tipo_servico');
+        return $this->hasOne(TipoServico::class, 'id', 'id_tipo_servico');
     }
 
 }
